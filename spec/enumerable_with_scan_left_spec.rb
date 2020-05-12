fdf007a57f377079938c79a694eb691652ccd2f5
Parker Finch <pfinch@panoramaed.com>
2020-05-12 17:23:04 -0400

    Skip EnumerableWithScanLeft specs on Ruby 2.7
    The specs fail with a `NoMethodError`, but it's a false
    error. Manually testing shows that the refinement still works on ruby
    2.7. We have created issues in both rspec and ruby, since it's not
    clear where the underlying issue lives:
    
    https://github.com/rspec/rspec-core/issues/2727
    https://bugs.ruby-lang.org/issues/16852

--- spec/enumerable_with_scan_left_spec.rb
+++ spec/enumerable_with_scan_left_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe EnumerableWithScanLeft do
 
   subject { enumerable.scan_left(initial, &block) }
 
-  include_examples "scan_left_examples"
+  # Right now there's a bug in either rspec or ruby that prevents
+  # these specs from passing on ruby 2.7. See:
+  #   * https://github.com/rspec/rspec-core/issues/2727
+  #   * https://bugs.ruby-lang.org/issues/16852
+  #
+  # Until this is resolved, we skip these specs on Ruby 2.7.
+  include_examples "scan_left_examples" unless RUBY_VERSION.start_with?("2.7")
 end
